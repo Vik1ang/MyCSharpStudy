eae20e6ad951645c18be97cac9991c913bdb71de
Vik1ang <vik1ang@outlook.com>
2022-02-26 20:15:07 +0800

    attribute

---
+++ MyAttribute/Extension/RemarkExtension.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+
+namespace MyAttribute.Extension;
+
+public static class RemarkExtension
+{
+    public static string GetRemark(this Enum value)
+    {
+        Type type = value.GetType();
+        FieldInfo? field = type.GetField(value.ToString());
+        if (field!.IsDefined(typeof(RemarkAttribute), true))
+        {
+            RemarkAttribute attribute = (RemarkAttribute)field.GetCustomAttribute(typeof(RemarkAttribute), true)!;
+            return attribute.GetRemark();
+        }
+
+        return value.ToString();
+    }
+}
+
+public enum UserState
+{
+    [Remark("正常")]
+    Normal = 0,
+
+    [Remark("正常")]
+    Frozen = 1,
+
+    // [Remark("正常")]
+    Deleted = 2
+}
+
+public class RemarkAttribute : Attribute
+{
+    private string _Remark;
+
+    public RemarkAttribute(string remark)
+    {
+        _Remark = remark;
+    }
+
+    public string GetRemark()
+    {
+        return _Remark;
+    }
+}
--- MyAttribute/Program.cs
+++ MyAttribute/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MyAttribute;
+﻿using MyAttribute.Extension;
+
+namespace MyAttribute;
 
 internal class Program
 {
@@ -6,22 +8,27 @@ internal class Program
     {
         try
         {
+            /* {
+                 Student student = new Student
+                 {
+                     Id = 123,
+                     Name = "time"
+                 };
+                 // student.Study();
+                 // string result = student.Answer("Eleven");
+                 Manager.Show(student);
+             }*/
+
             {
-                Student student = new Student
-                {
-                    Id = 123,
-                    Name = "time"
-                };
-                // student.Study();
-                // string result = student.Answer("Eleven");
-                Manager.Show(student);
+                Console.WriteLine(UserState.Normal.GetRemark());
+                Console.WriteLine(UserState.Frozen.GetRemark());
+                Console.WriteLine(UserState.Deleted.GetRemark());
             }
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
+            throw;
         }
-
-        Console.ReadKey();
     }
 }
