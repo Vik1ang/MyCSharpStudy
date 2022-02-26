6c77cbc660f6b50e13747857064e3d87ee519836
Vik1ang <vik1ang@outlook.com>
2022-02-26 20:41:21 +0800

    attribute

---
+++ MyAttribute/Extension/ValidateExtension.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+
+namespace MyAttribute.Extension;
+
+public static class ValidateExtension
+{
+    public static bool Validate(this object o)
+    {
+        Type type = o.GetType();
+        foreach (PropertyInfo propertyInfo in type.GetProperties())
+        {
+            if (propertyInfo.IsDefined(typeof(LongAttribute), true))
+            {
+                LongAttribute attribute = (LongAttribute)type.GetCustomAttribute(typeof(LongAttribute), true)!;
+                if (!attribute.Validate(propertyInfo.GetValue(o)))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
+public class LongAttribute : Attribute
+{
+    private long _Min = 0;
+    private long _Max = 0;
+
+    public LongAttribute(long _min, long _max)
+    {
+        _Min = _min;
+        _Max = _max;
+    }
+
+    public bool Validate(object? value)
+    {
+        if (value != null && !string.IsNullOrWhiteSpace(value.ToString()))
+        {
+            if (long.TryParse(value.ToString(), out long IResult))
+            {
+                if (IResult > _Min && IResult < _Max)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
