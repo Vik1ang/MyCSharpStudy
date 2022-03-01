95619f7e017784228af704db15f05b3bb2119bf0
Vik1ang <vik1ang@outlook.com>
2022-03-01 22:19:34 +0800

    delegate

---
+++ MyDelegateEvent/MyDelegate.cs
@@ -0,0 +1,25 @@
+﻿namespace MyDelegateEvent;
+
+public delegate void NoReturnNoParaOutClass();
+
+public class MyDelegate
+{
+    public delegate void NoReturnNoPara();
+
+    public delegate void NoReturnWithPara(int x, int y);
+
+    public delegate void WithReturnNoPara();
+
+    public delegate string WithReturnWithPara(out int x, ref int y);
+
+    public delegate void GenericNoReturnNoPara<T>();
+
+    public void Show()
+    {
+        Student student = new Student
+        {
+            Id = 96,
+            Name = "Summer"
+        };
+    }
+}
---
+++ MyDelegateEvent/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace MyDelegateEvent;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
