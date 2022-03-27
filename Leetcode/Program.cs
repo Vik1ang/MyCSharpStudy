dd06ce3f9c9fb7eda1b728632d14bbb36a13d352
Vik1ang <vik1ang@outlook.com>
2022-03-28 00:01:55 +0800

    lc

---
+++ Leetcode/Program.cs
@@ -0,0 +1,8 @@
+﻿using Leetcode.Solution.q1;
+
+int[] res = new Solution().TwoSum(new int[]{ 2, 7, 11, 15 }, 9);
+
+foreach (var re in res)
+{
+    Console.WriteLine(re);
+}
---
+++ Leetcode/Solution/q1/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode.Solution.q1;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> hashTable = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int value;
+            if (hashTable.TryGetValue(target - nums[i], out value))
+            {
+                return new int[] { value, i };
+            }
+            hashTable[nums[i]] = i;
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
