873f60cfd4a20f22f4759e2cadd89fa46f4c0b8a
Vik1ang <vik1ang@outlook.com>
2022-04-01 17:20:29 +0800

    lc

---
+++ Leetcode/Solution/q7/Solution.cs
@@ -0,0 +1,23 @@
+﻿using static System.Int32;
+
+namespace Leetcode.Solution.q7;
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        int rev = 0;
+        while (x != 0)
+        {
+            if (rev is < int.MinValue / 10 or > int.MaxValue / 10)
+            {
+                return 0;
+            }
+            int digit = x % 10;
+            x /= 10;
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
+}
